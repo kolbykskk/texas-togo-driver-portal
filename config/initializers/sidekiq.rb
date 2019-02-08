d000c8b5f1a4db238daa6a6cfeb6a4b7ef45853d
Kolby <kolbykalafut@gmail.com>
2019-02-08 09:32:41 -0500

    fix sidekiq on heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,12 @@
+require 'sidekiq'
+
+Sidekiq.configure_client do |config|
+   config.redis = { :size => 1 }
+end
+
 Sidekiq.configure_server do |config|
-    config.server_middleware do |chain|
-       chain.add Sidekiq::Middleware::Server::RetryJobs, :max_retries => 0
-    end
- end
+   config.redis = { :size => 4 }
+   config.server_middleware do |chain|
+      chain.add Sidekiq::Middleware::Server::RetryJobs, :max_retries => 0
+   end
+end
