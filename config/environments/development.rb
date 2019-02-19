168f532389e03d04152a8e936905a37f074dc231
Kolby <kolbykalafut@gmail.com>
2019-02-18 19:55:17 -0500

    add minor features

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,20 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+  :user_name => 'letsdodelivery@gmail.com',
+  :password => 'umehfupjcjqzbnin',
+  :domain => 'letsdodelivery.com',
+  :address => 'smtp.gmail.com',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
