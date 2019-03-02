28090f1f08017bbecf640b6026ea7d3a092ca78e
Kolby <kolbykalafut@gmail.com>
2019-03-02 17:56:57 -0500

    change allowed format for CSV file uploads

--- app/workers/process_payment_sheet_worker.rb
+++ app/workers/process_payment_sheet_worker.rb
@@ -6,19 +6,19 @@ class ProcessPaymentSheetWorker
     begin
       payment_sheet = PaymentSheet.last
       CSV.new(open(payment_sheet.sheet.url)).each do |row| 
-        user = User.find_by(phone_number: row[1])
+        user = User.find_by(phone_number: row[1].gsub(/[^\w\s]/, ''))
 
-          amount = (row[9].to_f * 100).to_i
-          commissions = (row[5].to_f * 100).to_i
-          tips = (row[6].to_f * 100).to_i
-          charges = (row[7].to_f * 100).to_i
-          credits = (row[8].to_f * 100).to_i
+          amount = (row[6].to_f * 100).to_i
+          commissions = (row[2].to_f * 100).to_i
+          tips = (row[3].to_f * 100).to_i
+          charges = (row[4].to_f * 100).to_i
+          credits = (row[5].to_f * 100).to_i
 
           if !user.nil? && !user.stripe_account.nil?
             payment_sheet.total_paid += amount
             stat = Stat.find_by(user_id: user.id)
             stat.days_worked += 1
-            stat.deliveries += row[10].to_i
+            stat.deliveries += row[7].to_i
             stat.save
             
             account_id = user.stripe_account 
@@ -29,17 +29,16 @@ class ProcessPaymentSheetWorker
                 destination: account_id
               }
             )
-            Disbursment.create(deliveries_made: row[10].to_i, commissions: commissions, tips: tips, charges: charges, credits: credits, payment_sheet_id: payment_sheet.id, driver_name: row[0], driver_phone: row[1], amount: amount)
+            Disbursment.create(deliveries_made: row[7].to_i, commissions: commissions, tips: tips, charges: charges, credits: credits, payment_sheet_id: payment_sheet.id, driver_name: row[0], driver_phone: row[1].gsub(/[^\w\s]/, ''), amount: amount)
             payment_sheet.number_of_drivers += 1 
           else
-            Disbursment.create(not_found: true, deliveries_made: row[10].to_i, commissions: commissions, tips: tips, charges: charges, credits: credits, payment_sheet_id: payment_sheet.id, driver_name: row[0], driver_phone: row[1], amount: amount)
+            Disbursment.create(not_found: true, deliveries_made: row[7].to_i, commissions: commissions, tips: tips, charges: charges, credits: credits, payment_sheet_id: payment_sheet.id, driver_name: row[0], driver_phone: row[1].gsub(/[^\w\s]/, ''), amount: amount)
             payment_sheet.not_found += 1
           end
       end
       payment_sheet.finished = true
       payment_sheet.save
     rescue => e
-      puts e
       payment_sheet = PaymentSheet.last
       payment_sheet.failed = true
       payment_sheet.save
