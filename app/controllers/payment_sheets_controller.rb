d417a8b8866b71ee529950a88f94598688d5e957
Kolby <kolbykalafut@gmail.com>
2019-11-14 12:10:40 -0500

    fix fuinds chekc

--- app/controllers/payment_sheets_controller.rb
+++ app/controllers/payment_sheets_controller.rb
@@ -1,19 +1,20 @@
 class PaymentSheetsController < ApplicationController
   def create
     @payment_sheet = PaymentSheet.new(payment_sheet_params)
-    if enough_funds_check
       if @payment_sheet.save
+        if enough_funds_check
           ProcessPaymentSheetWorker.perform_async
           flash[:success] = "Payment disbursment process successfully started in the background."
           redirect_to dashboard_path
+        else
+          flash[:error] = "There are not enough funds."
+          @payment_sheet.destroy
+          redirect_to dashboard_path
+        end
       else
         flash[:error] = "There was an error."
         redirect_to dashboard_path
       end
-    else
-      flash[:error] = "There are not enough funds."
-      redirect_to dashboard_path
-    end
   end
 
   def enough_funds_check
