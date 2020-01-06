b189096685e360354ac3fc1db5870bbef4dd5874
Kolby <kolbykalafut@gmail.com>
2020-01-05 19:46:03 -0500

    debugging

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -76,7 +76,7 @@ class WebhooksController < ApplicationController
         payout = event.data.object
 
         unless payout.method == "standard"
-          orig_amt_cents = payout.amount/(1-0.03).round
+          orig_amt_cents = payout.amount/(1-0.03)
           (orig_amt_cents*0.03).round >= 150 ? fee = (orig_amt_cents*0.03).round : fee = 150
 
           puts "FEE: #{fee}"
