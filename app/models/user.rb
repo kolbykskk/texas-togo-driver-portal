c8a796e8c6e2b24c6ad5fddbf60395c4250dce1c
Kolby <kolbykalafut@gmail.com>
2020-01-10 14:18:50 -0500

    working on zapier webhook

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ApplicationRecord
 
   validates_presence_of :first_name
   validates_presence_of :last_name
-  validates_presence_of :phone_number, uniqueness: true
+  validates_presence_of :phone_number
+  validates_uniqueness_of :phone_number
 
   def phone_number=(val)
     write_attribute(:phone_number, val.gsub(/[^\w\s]/, ''))
