68fcf55ac4042a40f543e3b147982e3af476aecd
Kolby <kolby@pop-os.localdomain>
2020-10-24 13:02:05 -0400

    env var fixes

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -5,7 +5,7 @@ class Stat < ApplicationRecord
   after_update :reached_x_deliveries, if: :deliveries_changed?
 
   def reached_x_deliveries
-    if user.referrer && deliveries_was < ENV["REFER_DELIVERIES"] && deliveries >= ENV["REFER_DELIVERIES"]
+    if user.referrer && deliveries_was < ENV["REFER_DELIVERIES"].to_i && deliveries >= ENV["REFER_DELIVERIES"].to_i
       PayReferralWorker.perform_async(self.user.stripe_account, self.user.referrer.stripe_account)
       self.user.update_attributes(refer_paid: true)
     end
